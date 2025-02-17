6155b096bbdf51b18ed618460329b08df008d7b5
jagarcia05 <joseantoniogarciapascual17@gmail.com>
2025-02-17 13:50:38 +0100

    commit final

--- mi-proyecto/src/App.tsx
+++ mi-proyecto/src/App.tsx
@@ -1,7 +1,7 @@
 import Header from "./components/header";
-import Card from "./components/card";
-import Card2 from "./components/card2";
-import Slider from "./components/slider";
+import CardActividad from "./components/CardActividad";
+import CardTrabajadores from "./components/CardTrabajadores";
+import Slider from "./components/Slider";
 
 function App() {
   return (
@@ -10,31 +10,31 @@ function App() {
 
       <main className="bg-secundary">
         <div className="container mx-auto grid grid-cols-5 gap-6 p-6 justify-center">
-          <Card
+          <CardTrabajadores
             imageSource="src/img/employee1.jpg"
             name="Ana Rodriguez"
             role="Directora creativa"
             description="Ana es la mente creativa detrás de nuestras campañas. Con una visión innovadora y una pasión por el diseño, transforma ideas en estrategias publicitarias impactantes."
           />
-          <Card
+          <CardTrabajadores
             imageSource="src/img/employee2.jpg"
             name="Carlos Méndez"
             role="Estratega de marketing"
             description="Carlos analiza tendencias y diseña planes de marketing efectivos para potenciar la presencia digital de nuestros clientes. Su enfoque estratégico garantiza resultados exitosos."
           />
-          <Card
+          <CardTrabajadores
             imageSource="src/img/employee3.jpg"
             name="Sofía García"
             role="Diseñadora gráfica"
             description="Sofía da vida a las ideas con diseños atractivos y funcionales. Su creatividad y atención al detalle hacen que cada campaña visual sea única y memorable."
           />
-          <Card
+          <CardTrabajadores
             imageSource="src/img/employee4.jpg"
             name="Javier Pérez"
             role="Analista de datos"
             description="Javier convierte los números en información valiosa. Con su habilidad en analítica digital, optimiza campañas y maximiza el impacto de cada estrategia publicitaria."
           />
-          <Card
+          <CardTrabajadores
             imageSource="src/img/employee5.jpg"
             name="Laura Martínez"
             role="Redactora de contenido"
@@ -44,27 +44,27 @@ function App() {
 
         <h1 className="text-h1 text-center">Lo que hacemos</h1>
         <div className="container mx-auto grid grid-cols-5 gap-6 p-6 justify-center">
-          <Card2
+          <CardActividad
             titulo="Estrategia de Marketing"
             titulo2="Planificación"
             description="Desarrollamos estrategias de marketing personalizadas para cada cliente, asegurando que cada campaña esté alineada con sus objetivos y alcance el público adecuado."
           />
-          <Card2
+          <CardActividad
             titulo="Diseño Gráfico"
             titulo2="Creatividad Visual"
             description="Creamos diseños gráficos impactantes que capturan la esencia de su marca y atraen la atención de su audiencia. Desde logotipos hasta material promocional."
           />
-          <Card2
+          <CardActividad
             titulo="Gestión de Redes Sociales"
             titulo2="Presencia Digital"
             description="Manejamos sus perfiles en redes sociales, creando contenido atractivo y gestionando la interacción con sus seguidores para máxima confianza"
           />
-          <Card2
+          <CardActividad
             titulo="Publicidad Digital"
             titulo2="Campañas"
             description="Diseñamos y ejecutamos campañas de publicidad digital en diversas plataformas, optimizando cada anuncio para maximizar el retorno de inversión."
           />
-          <Card2
+          <CardActividad
             titulo="Análisis de Datos"
             titulo2="Optimización"
             description="Utilizamos herramientas de análisis de datos para medir el rendimiento de sus campañas y ajustar las estrategias en tiempo real, asegurando resultados óptimos y eficientes."
